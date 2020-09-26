fcdc135a0a3d4c9a680373a0c916bf9a06fa89a9
SprutSDM <zakoylov@gmail.com>
2020-09-27 01:03:22 +0300

    Add marker layout

--- app/src/main/java/ru/zakoulov/navigatorx/ui/map/MapFragment.kt
+++ app/src/main/java/ru/zakoulov/navigatorx/ui/map/MapFragment.kt
@@ -5,41 +5,90 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import androidx.core.view.forEach
+import android.view.ViewTreeObserver
+import android.widget.TextView
 import androidx.core.view.forEachIndexed
 import androidx.fragment.app.Fragment
 import com.otaliastudios.zoom.ZoomEngine
 import kotlinx.android.synthetic.main.fragment_map.markers_layout
 import kotlinx.android.synthetic.main.fragment_map.zoom_layout
 import ru.zakoulov.navigatorx.R
+import kotlin.random.Random
 
 class MapFragment : Fragment(R.layout.fragment_map) {
 
-    private val markers = listOf(232 to 231, 669 to 440)
+    private val markersData = List(200) {
+        Marker(
+            x = Random.nextInt(IMAGE_WIDTH),
+            y = Random.nextInt(IMAGE_HEIGHT),
+            label = Random.nextInt(100, 10000).toString()
+        )
+    }
+
+    private var markers = mutableListOf<MarkerViewHolder>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val layoutInflater = LayoutInflater.from(view.context)
-        markers.forEach {
+        markersData.forEach {
             val view = layoutInflater.inflate(R.layout.marker, markers_layout, false)
+            val viewHolder = MarkerViewHolder(view).apply {
+                setupWith(it)
+            }
+            markers.add(viewHolder)
             markers_layout.addView(view)
         }
         zoom_layout.engine.addListener(object : ZoomEngine.Listener {
             override fun onIdle(engine: ZoomEngine) = Unit
 
             override fun onUpdate(engine: ZoomEngine, matrix: Matrix) {
-                markers_layout.forEachIndexed { index, markerView ->
-                    markerView.translationX =
-                        engine.scaledPanX + markers[index].first / 775f * engine.contentWidth * engine.realZoom
-                    markerView.translationY =
-                        engine.scaledPanY + markers[index].second / 614f * engine.contentHeight * engine.realZoom
+                markers.forEach {
+                    it.updatePosition(
+                        scaledPan = engine.scaledPanX to engine.scaledPanY,
+                        parentSize = engine.contentWidth to engine.contentHeight,
+                        zoom = engine.realZoom
+                    )
                 }
             }
         })
     }
 
+    private class MarkerViewHolder(val view: View) {
+        private val textView = view.findViewById<TextView>(R.id.marker_text)
+        private val markerPointer = view.findViewById<View>(R.id.marker_pointer)
+
+        private var positionX: Float = 0f
+        private var positionY: Float = 0f
+
+        private var xOffset: Float = 0f
+        private var yOffset: Float = 0f
+
+        fun setupWith(marker: Marker) {
+            textView.text = marker.label
+            positionX = marker.x.toFloat()
+            positionY = marker.y.toFloat()
+            view.viewTreeObserver.addOnGlobalLayoutListener(object: ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    view.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                    xOffset = view.width / 2f
+                    yOffset = markerPointer.bottom.toFloat()
+                }
+            })
+        }
+
+        fun updatePosition(scaledPan: Pair<Float, Float>, parentSize: Pair<Float, Float>, zoom: Float) {
+            view.apply {
+                translationX = scaledPan.first - xOffset + positionX / IMAGE_WIDTH * parentSize.first * zoom
+                translationY = scaledPan.second - yOffset + positionY / IMAGE_HEIGHT * parentSize.second * zoom
+            }
+        }
+    }
+
     companion object {
         const val TAG = "MapFragment"
+
+        private const val IMAGE_WIDTH = 4096
+        private const val IMAGE_HEIGHT = 4096
 
         val instance: MapFragment by lazy { MapFragment() }
     }
---
+++ app/src/main/java/ru/zakoulov/navigatorx/ui/map/Marker.kt
@@ -0,0 +1,7 @@
+package ru.zakoulov.navigatorx.ui.map
+
+data class Marker(
+    val x: Int,
+    val y: Int,
+    val label: String
+)
