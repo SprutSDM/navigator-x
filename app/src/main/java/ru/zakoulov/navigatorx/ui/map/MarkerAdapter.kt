8d0b9bcc2b1f27581ad739601598d0c946706410
SprutSDM <zakoylov@gmail.com>
2020-10-14 00:01:25 +0300

    Remove depth rate from layout

--- app/src/main/java/ru/zakoulov/navigatorx/ui/map/MarkerAdapter.kt
+++ app/src/main/java/ru/zakoulov/navigatorx/ui/map/MarkerAdapter.kt
@@ -25,10 +25,12 @@ class MarkerAdapter(private val data: List<Marker>) : ZoomMapAdapter<MarkerAdapt
     override fun getChildCount() = data.size
 
     class MarkerViewHolder(view: View) : ZoomMapViewHolder(view) {
+        private var id: Int = 0
         private var positionX: Float = 0f
         private var positionY: Float = 0f
         private var visibilityRate: Float = 0f
         private var markerPointer: View = view.findViewById(R.id.marker_pointer)
+        private var isVisible: Boolean = false
 
         private var label: TextView = view.findViewById(R.id.marker_text)
 
@@ -37,19 +39,58 @@ class MarkerAdapter(private val data: List<Marker>) : ZoomMapAdapter<MarkerAdapt
             positionY = markerData.y.toFloat()
             visibilityRate = markerData.depthRate
             label.text = markerData.label
+            id = markerData.id
+            view.visibility = View.INVISIBLE
             view.setOnClickListener {
                 Log.d(TAG, "setupViewHolder: onClicked ${markerData.label}")
             }
         }
 
-        override fun getVisibilityRate() = visibilityRate
         override fun getPositionX() = positionX
         override fun getPositionY() = positionY
         override fun getXPivot() = view.width / 2f
         override fun getYPivot() = markerPointer.bottom.toFloat()
+
+        override fun onVisibilityRateChanged(rate: Float) {
+            when {
+                !isVisible && rate >= visibilityRate -> showView()
+                isVisible && rate < visibilityRate - DEPTH_SHIFT -> hideView()
+            }
+        }
+
+        private fun showView() {
+            with(view) {
+                clearAnimation()
+                visibility = View.VISIBLE
+                alpha = 0f
+                animate()
+                    .setDuration(ANIMATION_TIME)
+                    .alpha(1f)
+                    .start()
+            }
+            isVisible = true
+        }
+
+        private fun hideView() {
+            with(view) {
+                clearAnimation()
+                visibility = View.VISIBLE
+                alpha = 1f
+                animate()
+                    .setDuration(ANIMATION_TIME)
+                    .alpha(0f)
+                    .withEndAction {
+                        view.visibility = View.INVISIBLE
+                    }
+                    .start()
+            }
+            isVisible = false
+        }
     }
 
     companion object {
         private const val TAG = "MarkerAdapter"
+        private const val ANIMATION_TIME = 200L
+        private const val DEPTH_SHIFT = 0.035f
     }
 }
